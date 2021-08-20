8a2224a5dae115ae95423854e730cf0a36cd718d
Gerardo Lucero <gera_conecta@hotmail.com>
2021-08-20 12:51:02 -0600

    scroll smooth

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -18,6 +18,13 @@
         <script src="{{ asset('js/app.js', true) }}" defer></script>
 
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css" integrity="sha512-1ycn6IcaQQ40/MKBW2W4Rhis/DbILU74C1vSrLJxCq57o941Ym01SwNsOMqvEBFlcgUa6xLiPY/NS5R+E6ztJQ==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+        
+        <style>
+            html {
+                scroll-behavior: smooth;
+            }
+        </style>
+    
     </head>
     <body class="font-sans antialiased">
         @inertia
