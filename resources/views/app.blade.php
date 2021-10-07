70613b833cbb794d2ab693329f92a6309da59f37
Gerardo Lucero <gera_conecta@hotmail.com>
2021-10-07 09:49:31 -0600

    se agrego el anali

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -16,6 +16,13 @@
         <!-- Scripts -->
         @routes
         <script src="{{ asset('js/app.js', true) }}" defer></script>
+        <!-- Google Tag Manager -->
+        <script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+        new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+        j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+        'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+        })(window,document,'script','dataLayer','GTM-KJBHKH2');</script>
+        <!-- End Google Tag Manager -->
 
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css" integrity="sha512-1ycn6IcaQQ40/MKBW2W4Rhis/DbILU74C1vSrLJxCq57o941Ym01SwNsOMqvEBFlcgUa6xLiPY/NS5R+E6ztJQ==" crossorigin="anonymous" referrerpolicy="no-referrer" />
         
